89d2ac65c760c9d1877418fff3fbd2eb345ef697
iWithman <simpw@mail.broward.edu>
2020-05-06 15:29:22 -0400

    Added background for the card, position contents in the card properly and added the icons.

--- src/components/Home/cardView.js
+++ src/components/Home/cardView.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone, faMapMarkerAlt, faGlobe  } from '@fortawesome/free-solid-svg-icons'
 import './home.scss'
 class CardView extends Component {
   state = {  }
@@ -8,22 +10,35 @@ class CardView extends Component {
         <div className='card-view-card'>
           <div className='card-logo'>iBC</div>
           <div className='card-name-profession'>
-            <div>Withman Simprevil</div>
-            <div>Software Developer</div>
+            <div>
+              <div className='card-name'>Withman Simprevil</div>
+              <div className='card-profession'>Software Developer</div>
+            </div>
+            
           </div>
-          <div className='card-adress'>
-            <div>South Florida</div>
-            <div>united states 12345</div>
+          <div className='card-adress infos'>
+            <FontAwesomeIcon className='card-icons' style={{cursor: 'pointer'}} size="lg" icon={faMapMarkerAlt} />
+            <div>
+              <div>South Florida</div>
+              <div>united states 12345</div>
+            </div>
           </div>
-          <div className='card-phone'>
+          <div className='card-phone infos'>
+            <FontAwesomeIcon className='card-icons' style={{cursor: 'pointer'}} size="lg" icon={faPhone} />
             <div>954-999-9999</div>
-            <div>phone 2</div>
           </div>
-          <div className='card-email-website'>
-            <div>withman.simprevil@gmail.com</div>
-            <div>wwww.iWithman.com</div>
+          <div className='card-email-website infos'>
+            <FontAwesomeIcon className='card-icons' style={{cursor: 'pointer'}} size="lg" icon={faGlobe} />
+            <div>
+              <div>withman.simprevil@gmail.com</div>
+              <div>wwww.iWithman.com</div>
+            </div>
           </div>
-          <div className='card-qr-code'>qr code</div>
+
+          <div className='card-qr-code-container'>
+            <div className='card-qr-shape'></div>
+            <div className='card-qr-code'>qr code</div>
+          </div>
         </div>
       </div>
     );
