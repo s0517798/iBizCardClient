b756ec30998a25ecff08746ba7fdc2abaaca6bf4
iWithman <simpw@mail.broward.edu>
2020-05-26 23:45:45 -0400

    You can now share the current card that is selected to different platforms.

--- src/components/Cards/shareCardModal.js
+++ src/components/Cards/shareCardModal.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShareAlt  } from '@fortawesome/free-solid-svg-icons'
+import {
+  EmailShareButton,
+  EmailIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  
+} from "react-share";
 import './cards.scss'
 
 const ShareCardModal = () => {
@@ -10,10 +23,31 @@ const ShareCardModal = () => {
       <div className="modal fade bd-example-modal-sm" tabIndex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
         <div className="modal-dialog modal-sm">
           <div className="modal-content">
-            <div>email</div>
-            <div>linkedin</div>
-            <div>facebook</div>
-            <div>twiter</div>
+            <div>
+              <EmailShareButton url={window.location.href}>
+                <EmailIcon size={32} round/>
+              </EmailShareButton>
+            </div>
+            <div>
+              <WhatsappShareButton url={window.location.href}>
+                <WhatsappIcon size={32} round/>
+              </WhatsappShareButton>
+            </div>
+            <div>
+              <FacebookShareButton url={window.location.href}>
+                <FacebookIcon size={32} round/>
+              </FacebookShareButton>
+            </div>
+            <div>
+              <TwitterShareButton url={window.location.href}>
+                <TwitterIcon size={32} round/>
+              </TwitterShareButton>
+            </div>
+            <div>
+              <LinkedinShareButton url={window.location.href}>
+                <LinkedinIcon size={32} round/>
+              </LinkedinShareButton>
+            </div>
           </div>
         </div>
       </div>
